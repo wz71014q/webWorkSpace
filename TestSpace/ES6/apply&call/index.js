c4fb8dbf9518e1c834e5d3c834c0e29dffcfffc4
Qiang <710147356@qq.com>
2018-05-03 16:15:01 +0800

    addshoplist

---
+++ TestSpace/ES6/apply&call/index.js
@@ -0,0 +1,18 @@
+function init () {
+  var student = new Student('qian', 21, '一年级')
+  console.log('student = ' + student)
+  console.log('name = ' + student.name + ', age = ' + student.age + ', grade = ' + student.age)
+}
+function Person (name, age) {
+    this.name = name
+    this.age = age
+    console.log(this)
+  }
+  function Student (name, age, grade) {
+    Person.apply(this, arguments)
+    this.grade = grade
+    console.log(this)
+  }
+window.onload = function () {
+  init()
+}
