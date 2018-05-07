d64d992306f291aac6c151667339ca1028f23e52
Qiang <710147356@qq.com>
2018-05-07 14:42:57 +0800

    重装eslint

--- TestSpace/ES6/let&const/let.js
+++ TestSpace/ES6/let&const/let.js
@@ -1,14 +1,8 @@
 function init() {
   var tmp = 123;
   console.log('init');
-  if (true) {
-    tmp = 'abc';
-    let tmp;
-  }
   console.log(tmp);
-}
-function f () {
 }
 Window.onload = function () {
   init();
-}
+};
