c8dfe73acbd74891adc52cfe6bd790798e09c46c
Qiang <710147356@qq.com>
2018-05-07 16:27:54 +0800

    块级作用域里定义函数

--- TestSpace/ES6/let&const/let.js
+++ TestSpace/ES6/let&const/let.js
@@ -2,6 +2,13 @@ function init() {
   var tmp = 123;
   console.log(`init, ${tmp}`);
 }
+function letFunc() {
+  (function () {
+    function init() { console.log('I am inside!'); }
+    init();
+  }());
+}
 window.onload = function () {
-  init();
+  // init();
+  letFunc();
 };
