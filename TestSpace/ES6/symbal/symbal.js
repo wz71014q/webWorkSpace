d66540db6023b153a3c66636f80575eec147f4b6
Qiang <710147356@qq.com>
2018-05-15 15:30:46 +0800

    ES6语法学习

---
+++ TestSpace/ES6/symbal/symbal.js
@@ -0,0 +1,8 @@
+/** *******************字符的unicode表示法**************** */
+let unisym = '\u{20BB7}';
+console.log(unisym);
+let comSting = 'Across this street and then the 1 Avenue will in front of you';
+console.log(comSting.includes('is', 15));// includes检测字符是否在字符串comString中，第二个参数是从什么位置开始检测直到结束
+console.log(comSting.repeat(3));// 重复3次
+console.log(`this is 1st line 1
+this is 2ed line 2`);
