91467f2a86031f7dc196ed9b331350c8b645dced
Qiang <710147356@qq.com>
2019-02-21 21:52:23 +0800

    更新wenbpack服务器配置

--- projects/webpackSpace/main.js
+++ projects/webpackSpace/main.js
@@ -1,1 +1,4 @@
 document.write('<h1>Hello WebPack</h1>');
+document.write('<h1>Hello world</h1>');
+document.write('<h1>Hello WebPackServer</h1>');
+document.write('<h1>Hello WebWorkSpace</h1>');
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const devServer = require('./devServer');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -11,11 +13,7 @@ module.exports = {
     filename: "bundle.js"//打包后输出文件的文件名
   },
   devtool: 'source-map',
-  devServer: {
-    contentBase: "/dist",//本地服务器所加载的页面所在的目录
-    historyApiFallback: true,//不跳转
-    inline: true//实时刷新
-  },
+  devServer: devServer,
   module: {
     rules: [
       {
@@ -56,6 +54,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: __dirname + '/index.html' // new 一个这个插件的实例，并传入相关的参数
     }),
-    new webpack.HotModuleReplacementPlugin()// 热加载插件
+    new webpack.HotModuleReplacementPlugin(),// 热加载插件
+    new FriendlyErrorsPlugin({
+      compilationSuccessInfo: {
+        messages: [`Your application is running: http://${devServer.host}:${devServer.port}`],
+      },
+    })
   ],
 }
