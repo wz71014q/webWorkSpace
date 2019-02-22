92fb76ea97799eb4432c5762419ead1b4e9781c1
WZQ <710147356@qq.com>
2019-02-22 09:45:06 +0800

    add cmd

--- webpack/server.js
+++ webpack/server.js
@@ -1,11 +1,15 @@
 const program = require('commander');
 
 program
-  .version('0.1.0')
-  .option('-a, --peppers', 'Add peppers')
+  .command('val <val> [prop]')
+  .option('-a, --val <val>', 'Add peppers')
   .option('-p, --pineapple', 'Add pineapple')
   .option('-b, --bbq-sauce', 'Add bbq sauce')
   .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .action((val, cmd) => {
+    console.log(val);
+    console.log(cmd);
+  })
   .parse(process.argv);
 
 console.log('you ordered a pizza with:');
@@ -13,3 +17,5 @@ if (program.peppers) console.log('  - peppers');
 if (program.pineapple) console.log('  - pineapple');
 if (program.bbqSauce) console.log('  - bbq');
 console.log('  - %s cheese', program.cheese);
+
+program.parse(process.argv);
