d3ed70a7856c1d7720da5341f5647f8555b07402
WZQ <710147356@qq.com>
2019-03-04 17:24:42 +0800

    新增build配置

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,13 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
+  // context: '',
   entry:  {
-    main: __dirname + "/projects/webpackSpace/world/main.js", // 入口文件
+    main: path.resolve(__dirname, 'projects/webpackSpace', 'world', 'main.js'), // 入口文件
   },
   output: {
-    path: __dirname + "/dist",//打包后的文件存放的地方
+    // publicPath: '',
+    path: path.join(__dirname, '/dist'),//打包后的文件存放的地方
     filename: "bundle.js"//打包后输出文件的文件名
   },
   resolve: {
@@ -20,8 +22,8 @@ module.exports = {
       '@': path.resolve('src')
     }
   },
-  // devtool: 'source-map',
-  devServer: devServer,
+  devtool: 'source-map',
+  // devServer: devServer,
   module: {
     rules: [
       {
@@ -60,13 +62,13 @@ module.exports = {
   plugins: [
     new webpack.BannerPlugin('版权所有，翻版必究'),
     new HtmlWebpackPlugin({
-      template: __dirname + '/index.html' // new 一个这个插件的实例，并传入相关的参数
+      template: path.resolve(__dirname, 'index.html') // new 一个这个插件的实例，并传入相关的参数
     }),
     new webpack.HotModuleReplacementPlugin(),// 热加载插件
-    new FriendlyErrorsPlugin({
-      compilationSuccessInfo: {
-        messages: [`Your application is running: http://${devServer.host}:${devServer.port}\n`],
-      },
-    }),
+    // new FriendlyErrorsPlugin({
+    //   compilationSuccessInfo: {
+    //     messages: [`Your application is running: http://${devServer.host}:${devServer.port}\n`],
+    //   },
+    // }),
   ],
 }
--- webpack/build/index.js
+++ webpack/build/index.js
@@ -1,33 +1,47 @@
 const webpack = require('webpack');
 const program = require('commander');
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 const baseConfig = require('../config/webpack.base.config');
-const serverConfig = require('../server/devServer');
 const merge = require('webpack-merge');
+const ora = require('ora');
 
-let entry;
+const spinner = ora({
+  spinner: {
+    frames:['←','↑','→','↓'],
+    interval: 80,
+  },
+  color: 'red',
+  text: 'Loading...',
+});
 
 program
   .command('val <val>')
   .action((val) => {
-    entry = path.resolve(__dirname, '../../', 'projects/webpackSpace', val, 'main.js');
+    const entry = path.resolve(__dirname, '../../', 'projects/webpackSpace', val, 'main.js');
     const inlineConfig = merge(baseConfig, {
       entry: function setEntry() {
         return entry; // 入口文件
-        // return entry; // 入口文件
       },
       mode: 'production',
-      devServer: serverConfig,
       plugins: [
         new FriendlyErrorsPlugin({
           compilationSuccessInfo: {
             messages: ['Your application build successed\n'],
           },
-        })
+        }),
+        new CleanWebpackPlugin([path.resolve('./dist')])
       ],
     });
-    webpack(inlineConfig);
+    spinner.start();
+    webpack(inlineConfig, (err, stats) => {
+      spinner.stop();
+      if (err) {
+        console.log(err);
+      }
+      console.log('build successed');
+    });
   });
 
 program.parse(process.argv);
