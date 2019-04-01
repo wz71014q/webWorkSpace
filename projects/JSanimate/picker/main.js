e61072c61566a3dcd9c19686374734cd8e9a760b
Qiang <710147356@qq.com>
2019-04-01 22:23:57 +0800

    update picker rotate

--- projects/JSanimate/picker/main.js
+++ projects/JSanimate/picker/main.js
@@ -15,20 +15,31 @@ const handleManager = {
 class CubeObject {
   constructor() {
     this.itemContent = document.getElementsByClassName('container')[0];
-    this.itemWidth = this.itemContent.width;
+    this.itemWidth = window.getComputedStyle(this.itemContent).width;
   }
 }
 const container = new CubeObject();
-console.log(container.itemWidth);
+let startPoint = 0;
 const touchEvent = {
   touchStart(eve) {
     let _eve = eve || window.event;
+    startPoint = _eve.clientX;
     console.log('touchStart', _eve.clientX);
+    console.log('startPoint', startPoint);
     handleManager.addHandle(container.itemContent, handleManager.moveEvent, touchEvent.touching);
   },
   touching(eve) {
     let _eve = eve || window.event;
-    console.log('touching', _eve.clientX);
+    const movingPoint = _eve.clientX;
+    const direction = movingPoint > startPoint ? 1 : -1;
+    if (direction > 0) {
+      addClass(container.itemContent, 'turnLeft');
+      removeClass(container.itemContent, 'turnRight');
+    } else {
+      addClass(container.itemContent, 'turnRight');
+      removeClass(container.itemContent, 'turnLeft');
+    }
+    console.log('touching', direction);
   },
   touchEnd(eve) {
     let _eve = eve || window.event;
@@ -38,3 +49,22 @@ const touchEvent = {
 };
 handleManager.addHandle(container.itemContent, handleManager.startEvent, touchEvent.touchStart);
 handleManager.addHandle(container.itemContent, handleManager.stopEvent, touchEvent.touchEnd);
+
+function hasClass(obj, cls) {
+  return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+function addClass(obj, cls) {
+  if (!hasClass(obj, cls)) {
+    obj.className += ` ${cls}`;
+  }
+}
+function removeClass(obj, cls) {
+  if (hasClass(obj, cls)) {
+    const reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+    obj.className = obj.className.replace(reg, ' ');
+  }
+}
+// function rotate(obj, dir) {
+//   let angle = 0;
+//   obj.style.transform = `rotate3d(0, 1, 0, ${angle}deg)`;
+// }
