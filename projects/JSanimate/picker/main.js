cdcdec943bf79d0d4f588f7bb1ac5517e934a4ed
WZQ <710147356@qq.com>
2019-04-12 17:37:39 +0800

    提出iemID

--- projects/JSanimate/picker/main.js
+++ projects/JSanimate/picker/main.js
@@ -3,10 +3,42 @@ import './style.css';
 
 const pickerWrapper = document.createElement('div');
 let startPoint = [];
-let angle = 0; // 转的角度
-let angleCache = 0; // 角度缓存
-const riadio = 300; // 半径
-let itemAngle = 0; // 元素分布角度
+const picker = {
+  angle: 0, // 整体转的角度
+  angleCache: 0, // 角度缓存
+  riadio: 300, // 元素半径
+  itemAngle: 0, // 元素分布角度
+  itemCount: 0, // 元素数量
+  itemWidth: 200, // 元素宽度
+  itemMaxHeight: 200, // 元素最大高度
+  itemId: 0 // 选择ID
+};
+function init(count) {
+  picker.itemCount = count;
+  picker.itemAngle = 360 / count;
+  picker.itemMaxHeight = Math.round(2 * Math.tan(picker.itemAngle / 2 * Math.PI / 180) * picker.riadio) - 20; // 元素最大高度
+  document.getElementById('app').appendChild(document.createElement('div'));
+  classManager.addClass(document.querySelectorAll('div')[1], 'container');
+  setStyle(count);
+  handleManager.addHandle(pickerWrapper, handleManager.startEvent, touchEvent.touchStart);
+}
+function setStyle(count) {
+  const container = document.getElementsByClassName('container')[0];
+  classManager.addClass(pickerWrapper, 'picker-wrapper');
+  container.appendChild(pickerWrapper);
+  pickerWrapper.style.width = `${picker.itemWidth}px`;
+  pickerWrapper.style.height = `${picker.itemMaxHeight}px`;
+  for (let i = 0; i < count; i += 1) {
+    pickerWrapper.appendChild(document.createElement('figure'));
+    classManager.addClass(document.querySelectorAll('figure')[i], 'item');
+    document.querySelectorAll('figure')[i].appendChild(document.createElement('p'));
+    document.querySelectorAll('p')[i].innerHTML = i;
+    classManager.addClass(document.querySelectorAll('p')[i], 'item-text');
+    document.querySelectorAll('figure')[i].style.width = `${picker.itemWidth}px`;
+    document.querySelectorAll('figure')[i].style.height = `${picker.itemMaxHeight}px`;
+    document.querySelectorAll('figure')[i].style.transform = `rotateX(${360 / count * i}deg) translateZ(${picker.riadio}px)`;
+  }
+}
 const touchEvent = {
   touchStart(eve) {
     let _eve = eve || window.event;
@@ -23,25 +55,35 @@ const touchEvent = {
     const movingPoint = [_eve.clientX || _eve.targetTouches[0].pageX, _eve.clientY || _eve.targetTouches[0].pageY];
     let verticalDistance = 0;
     verticalDistance = startPoint[1] - movingPoint[1];
-    rotate(pickerWrapper, verticalDistance);
+    handleManager.rotate(pickerWrapper, verticalDistance);
   },
   touchEnd(eve) {
     let _eve = eve || window.event;
     _eve.preventDefault();
     _eve.stopPropagation();
     const endPoint = [_eve.clientX || _eve.changedTouches[0].pageX, _eve.clientY || _eve.changedTouches[0].pageY];
-    angleCache += Math.round((startPoint[1] - endPoint[1]) * 3 / (5 * Math.PI));
+    picker.angleCache += Math.round((startPoint[1] - endPoint[1]) * 3 / (5 * Math.PI));
 
-    let angleCount = Math.abs(parseInt(angle / itemAngle, 10));
-    if (Math.abs(angle % 36) <= 18) { // 未满20%，返回原位置
-      angleCache = angle / Math.abs(angle) * itemAngle * angleCount;
-      rotate(pickerWrapper, '', angle / Math.abs(angle) * itemAngle * angleCount);
+    let angleCount = Math.abs(parseInt(picker.angle / picker.itemAngle, 10));
+    if (Math.abs(picker.angle % 36) <= 18) { // 未满20%，返回原位置
+      picker.angleCache = picker.angle / Math.abs(picker.angle) * picker.itemAngle * angleCount;
+      handleManager.rotate(pickerWrapper, '', picker.angle / Math.abs(picker.angle) * picker.itemAngle * angleCount);
     } else { // 超过20%，返回+1原位置
-      angleCache = angle / Math.abs(angle) * itemAngle * (angleCount + 1);
-      rotate(pickerWrapper, '', angle / Math.abs(angle) * itemAngle * (angleCount + 1));
+      picker.angleCache = picker.angle / Math.abs(picker.angle) * picker.itemAngle * (angleCount + 1);
+      handleManager.rotate(pickerWrapper, '', picker.angle / Math.abs(picker.angle) * picker.itemAngle * (angleCount + 1));
     }
-    clear();
+
+    picker.itemId = Math.abs(picker.angleCache / picker.itemAngle) % picker.itemCount;
+    if (picker.angleCache >= 0 && picker.itemId % 10 !== 0) {
+      picker.itemId = picker.itemCount - picker.itemId;
+    }
+    console.log('picker.itemId', picker.itemId);
+    touchEvent.clear();
   },
+  clear() {
+    handleManager.removeHandle(pickerWrapper, handleManager.touchStart, touchEvent.touchStart);
+    handleManager.removeHandle(document, handleManager.moveEvent, touchEvent.touching);
+  }
 };
 const handleManager = {
   startEvent: 'ontouchstart' in window ? 'touchstart' : 'mousedown',
@@ -53,43 +95,24 @@ const handleManager = {
   },
   removeHandle(obj, event, handle) {
     obj.removeEventListener(event, handle, false);
+  },
+  rotate(obj, verticalDistance, addAngle) {
+    if (addAngle !== undefined) {
+      picker.angle = addAngle;
+    } else {
+      picker.angle = Math.round(verticalDistance * 3 / (5 * Math.PI)) + picker.angleCache;
+    }
+    obj.style.transform = `rotateX(${picker.angle}deg)`;
   }
 };
-function clear() {
-  handleManager.removeHandle(pickerWrapper, handleManager.touchStart, touchEvent.touchStart);
-  handleManager.removeHandle(document, handleManager.moveEvent, touchEvent.touching);
-}
-function addClass(obj, cls) {
-  if (!hasClass(obj, cls)) {
-    obj.className += ` ${cls}`;
+const classManager = {
+  addClass(obj, cls) {
+    if (!this.hasClass(obj, cls)) {
+      obj.className += ` ${cls}`;
+    }
+  },
+  hasClass(obj, cls) {
+    return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
   }
-}
-function hasClass(obj, cls) {
-  return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
-}
-function rotate(obj, verticalDistance, addAngle) {
-  if (addAngle !== undefined) {
-    angle = addAngle;
-  } else {
-    angle = Math.round(verticalDistance * 3 / (5 * Math.PI)) + angleCache;
-  }
-  obj.style.transform = `rotateX(${angle}deg)`;
-}
-function init(count) {
-  itemAngle = 360 / count;
-  document.getElementById('app').appendChild(document.createElement('div'));
-  addClass(document.querySelectorAll('div')[1], 'container');
-  const container = document.getElementsByClassName('container')[0];
-  addClass(pickerWrapper, 'picker-wrapper');
-  container.appendChild(pickerWrapper);
-  for (let i = 0; i < count; i += 1) {
-    pickerWrapper.appendChild(document.createElement('figure'));
-    addClass(document.querySelectorAll('figure')[i], 'item');
-    document.querySelectorAll('figure')[i].appendChild(document.createElement('p'));
-    document.querySelectorAll('p')[i].innerHTML = i;
-    addClass(document.querySelectorAll('p')[i], 'item-text');
-    document.querySelectorAll('figure')[i].style.transform = `rotateX(${360 / count * i}deg) translateZ(${riadio}px)`;
-  }
-  handleManager.addHandle(pickerWrapper, handleManager.startEvent, touchEvent.touchStart);
-}
+};
 init(10);
