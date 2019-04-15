9310b8897705d8702d9260b4ba09c2c4fba2889c
WZQ <710147356@qq.com>
2019-04-15 10:15:20 +0800

    整理目录，删除多余文件

--- projects/basic/9.29/act_9_29.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-var reg=new RegExp('\\bis\\b','gi');
-var rep='He is a boy. IT IS a dog. Where is she?'.replace(reg,'I');
-document.write(rep);
-var reg1=/\w/;
-var reg2=/\w/g;
