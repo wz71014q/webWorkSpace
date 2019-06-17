6f3fb083373945d375fe32d77818dcb6f52e57b0
WZQ <710147356@qq.com>
2019-06-17 19:31:51 +0800

    add sinon

---
+++ test/sinon.test.js
@@ -0,0 +1,23 @@
+const sinon = require('sinon');
+
+it('sinon的用法', () => {
+  const spy = sinon.spy();
+  spy('hello');
+  console.log(spy.firstCall.args);
+  
+  let $route = {
+    setName() {
+      jest.fn();
+      // console.log('name: ', this.name);
+    }
+  };
+  // 为setName方法创建一个spy
+  let setNameSpy = sinon.spy($route, 'setName');
+  // 现在开始，每次调用这个方法时，相关信息都会被记录下来
+  $route.setName('Darth Vader');
+  // 通过spy对象可以查看这些记录的信息
+  console.log(setNameSpy.callCount); // output: 1
+  // 重要的最后一步，移除spy
+  setNameSpy.restore();
+})
+
