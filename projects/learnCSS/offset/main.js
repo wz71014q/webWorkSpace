dbf0a65d16ed1d4efa26b7c3a5a6f42f21855529
Qiang <710147356@qq.com>
2019-06-26 23:40:01 +0800

    update offset

--- projects/learnCSS/offset/main.js
+++ projects/learnCSS/offset/main.js
@@ -7,9 +7,11 @@ function init() {
   console.log('wrapper的offsetHeight: ' + wrapper.offsetHeight + ': ' + typeof wrapper.offsetHeight);
   console.log('wrapper的offsetWidth: ' + wrapper.offsetWidth + ': ' + typeof wrapper.offsetWidth);
   console.log('wrapper的offsetTop: ' + wrapper.offsetTop + ': ' + typeof wrapper.offsetTop);
-  console.log('content的offsetHeight: ' + content.offsetHeight + ': ' + typeof wrapper.offsetHeight);
-  console.log('content的offsetWidth: ' + content.offsetWidth + ': ' + typeof wrapper.offsetWidth);
-  console.log('content的offsetTop: ' + content.offsetTop + ': ' + typeof wrapper.offsetTop);
+  console.log('wrapper的offsetLeft: ' + wrapper.offsetLeft + ': ' + typeof wrapper.offsetLeft);
+  console.log('\n\ncontent的offsetHeight: ' + content.offsetHeight + ': ' + typeof content.offsetHeight);
+  console.log('content的offsetWidth: ' + content.offsetWidth + ': ' + typeof content.offsetWidth);
+  console.log('content的offsetTop: ' + content.offsetTop + ': ' + typeof content.offsetTop);
+  console.log('content的offsetLeft: ' + content.offsetLeft + ': ' + typeof content.offsetLeft);
 }
 
 init();
