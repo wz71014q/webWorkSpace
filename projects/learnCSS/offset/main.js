368921f01e8cd5c4228d54055f3eed98f693a50a
Qiang <710147356@qq.com>
2019-06-28 23:01:34 +0800

    update offset

--- projects/learnCSS/offset/main.js
+++ projects/learnCSS/offset/main.js
@@ -10,7 +10,10 @@ function init() {
   console.log('wrapper的offsetLeft: ' + wrapper.offsetLeft + ': ' + typeof wrapper.offsetLeft);
   console.log('wrapper的clientWidth: ' + wrapper.clientWidth + ': ' + typeof wrapper.clientWidth);
   console.log('wrapper的clientHeight: ' + wrapper.clientHeight + ': ' + typeof wrapper.clientHeight);
-  console.log(getComputedStyle(wrapper).width)
+  console.log('wrapper的scrollHeight: ' + wrapper.scrollHeight + ': ' + typeof wrapper.scrollHeight);
+  console.log('wrapper的scrollWidth: ' + wrapper.scrollWidth + ': ' + typeof wrapper.scrollWidth);
+  console.log('wrapper的scrollTop: ' + wrapper.scrollTop + ': ' + typeof wrapper.scrollTop);
+  console.log('wrapper的scrollLeft: ' + wrapper.scrollLeft + ': ' + typeof wrapper.scrollLeft);
 
   console.log('\n\ncontent的offsetHeight: ' + content.offsetHeight + ': ' + typeof content.offsetHeight);
   console.log('content的offsetWidth: ' + content.offsetWidth + ': ' + typeof content.offsetWidth);
@@ -19,6 +22,7 @@ function init() {
   console.log('content的clientWidth: ' + content.clientWidth + ': ' + typeof content.clientWidth);
   console.log('content的clientHeight: ' + content.clientHeight + ': ' + typeof content.clientHeight);
 
+  content.scrollTop = content.clientHeight;
   console.log('content的scrollHeight: ' + content.scrollHeight + ': ' + typeof content.scrollHeight);
   console.log('content的scrollWidth: ' + content.scrollWidth + ': ' + typeof content.scrollWidth);
   console.log('content的scrollTop: ' + content.scrollTop + ': ' + typeof content.scrollTop);
@@ -27,3 +31,29 @@ function init() {
 
 init();
 
+/**
+ * @description 获取浏览器缩放比例
+ */
+function detectZoom (){
+  let ratio = 0;
+  let screen = window.screen; // 用户屏幕信息
+  let ua = navigator.userAgent.toLowerCase(); // 浏览器信息，IE11以下包含msieXX
+  if (window.devicePixelRatio !== undefined) {
+    ratio = window.devicePixelRatio;
+    // 判断IE11以下，ua是否包含msie，等同于if (-1*ua.indexOf('msie') <= 0)
+  } else if (~ua.indexOf('msie')) {
+    if (screen.deviceXDPI && screen.logicalXDPI) {
+      // ratio = 物理像素 / 逻辑像素;
+      ratio = screen.deviceXDPI / screen.logicalXDPI;
+    }
+  } else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
+    ratio = window.outerWidth / window.innerWidth;
+  }
+
+   if (ratio) {
+    ratio = Math.round(ratio * 100);
+  }
+   return ratio;
+};
+let result = detectZoom()
+console.log(result);
