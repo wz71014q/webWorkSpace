bcda0f9d380e144bb4bc704f2d2db70c765e031c
wz2131q <zhiqiang2726@163.com>
2020-03-17 10:29:46 +0800

    fix: css

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,8 @@
 module.exports = {
   processors: [],
   plugins: ['stylelint-order', 'stylelint-config-rational-order/plugin'],
-  extends: ['stylelint-config-standard'], // 这是官方推荐的方式
+  extends: ['stylelint-config-standard'],
   rules: {
-    // 这里自定义规则
-    // 可参考官方：https://stylelint.docschina.org/user-guide/rules/
     'at-rule-no-unknown': [true,
       {
         ignoreAtRules: ['include', 'mixin', 'extend']
@@ -14,7 +12,6 @@ module.exports = {
     'at-rule-empty-line-before': 'never',
     'font-family-no-missing-generic-family-keyword': null,
     'no-descending-specificity': null,
-    // 属性排序规则: https://github.com/constverum/stylelint-config-rational-order
     'order/properties-order': [],
     'plugin/rational-order': [true,
       {
