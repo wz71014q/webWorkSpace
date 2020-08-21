8550cdad804e8b739b737eaf1c1f1a5eff13dcc3
wz2131q <zhiqiang2726@163.com>
2020-08-21 17:38:59 +0800

    feat: 视频库videojs预研

--- projects/vue/video/App.vue
+++ projects/vue/video/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="wrapper">
     <video
-      ref="videoPlayer"
-      id="player"
+      ref="videojsPlayer"
       webkit-playsinline
-      class="video-js vjs-styles-defaults vjs-default-skin vjs-big-play-centered">
+      class="videojsPlayer video-js vjs-styles-defaults vjs-default-skin vjs-big-play-centered">
+      <source src="https://smartsitblob01.blob.core.windows.net/smart-ilearning/MLA_Mandarin_7mb.mp4" />
+    </video>
+    <video
+      ref="videoPlayer"
+      controls
+      class="videoPlayer">
       <source src="https://smartsitblob01.blob.core.windows.net/smart-ilearning/MLA_Mandarin_7mb.mp4" />
     </video>
     <button class="video-btn">点我试试</button>
@@ -28,7 +33,8 @@ export default {
     }
   },
   mounted() {
-    this.initLib()
+    this.init();
+    this.initLib();
   },
   beforeDestroy() {
     if (this.player) {
@@ -37,8 +43,10 @@ export default {
   },
   methods: {
     init() {
-      const videoEle = document.querySelector('.product-video')
+      console.log(navigator.userAgent)
+      const videoEle = this.$refs.videoPlayer
       console.log(videoEle.volume)
+      console.log(videoEle.currentTime)
       videoEle.addEventListener('play', () => {
         // videoEle.currentTime = 5.0
         console.log(videoEle.duration)
@@ -46,8 +54,8 @@ export default {
           // videoEle.requestFullscreen();
         }
       })
-      videoEle.addEventListener('seeking', () => {
-        // console.log('seeking', e)
+      videoEle.addEventListener('seeking', (e) => {
+        console.log('seeking', e)
       })
       videoEle.addEventListener('seeked', (e) => {
         console.log('seeked', e)
@@ -66,6 +74,7 @@ export default {
       document.querySelector('.video-btn').addEventListener('click', () => {
         console.log('seeeeeeeeeeeeeeeeeeeeeeeeked')
         videoEle.currentTime = 20.0
+        videoEle.play()
       })
       document.querySelector('.add').addEventListener('click', () => {
         console.log('add')
@@ -81,12 +90,13 @@ export default {
       })
     },
     initLib () {
-      this.player = videojs(this.$refs.videoPlayer, {
+      this.player = videojs(this.$refs.videojsPlayer, {
+        usingNativeControls_: true,
         controls: true, // 是否显示控制条
+        loop: false, // 循环播放
         preload: 'auto',
         autoplay: false,
         fluid: true, // 自适应宽高
-        language: 'zh-CN', // 设置语言
         muted: false, // 是否静音
         inactivityTimeout: false,
         controlBar: { // 设置控制条组件 /* 设置控制条里面组件的相关属性及显示与否 */
@@ -98,6 +108,9 @@ export default {
             inline: false
           }, /* 使用children的形式可以控制每一个控件的位置，以及显示与否 */
           children: [
+            // {name: "PictureInPictureToggle"}, // 画中画模式
+            { name: "Menu" },
+            { name: "MenuButton" },
             { name: 'playToggle' }, // 播放按钮
             { name: 'currentTimeDisplay' }, // 当前已播放时间
             { name: 'progressControl' }, // 播放进度条
@@ -123,7 +136,7 @@ export default {
   width: 100%;
   height: 100%;
 }
-.product-video {
+.videoPlayer {
   z-index: 3;
   width: 100%;
   height: 285px;
