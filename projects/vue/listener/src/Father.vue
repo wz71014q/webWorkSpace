ea632997f829ecd4c86021ff3fcc5cfea629d3a3
WZQ <710147356@qq.com>
2023-02-15 11:14:09 +0800

    feat: 更新listeners demo

---
+++ projects/vue/listener/src/Father.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="father">
+    I'm the father
+    <Child @update="update"/>
+  </div>
+</template>
+
+<script>
+import Child from './Child.vue';
+
+export default {
+  name: 'Father',
+  components: { Child },
+  methods: {
+    update(val) {
+      console.log('I am the father', val);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.father {
+  padding: 30px;
+  border: 1px solid #000;
+}
+</style>
---
+++ projects/vue/listener/src/Grandson.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="grandson">
+    I'm the Grandson
+    <div @click="toFather">click me</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Grandson',
+  data() {
+    return {
+      msg: 'I am the Grandson',
+    };
+  },
+  methods: {
+    toFather() {
+      this.$emit('update', this.msg);
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.grandson {
+  padding: 30px;
+  border: 1px solid #f00;
+}
+</style>
